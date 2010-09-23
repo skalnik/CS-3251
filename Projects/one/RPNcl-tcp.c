18643355f1887a190a9a4b0c6262026b521a7b53
Mike Skalnik <mike.skalnik@gmail.com>
2010-09-23 11:20:18 -0400

    Working tcp version

--- Projects/one/RPNcl-tcp.c
+++ Projects/one/RPNcl-tcp.c
@@ -1,3 +1,4 @@
+/* Based on http://cs.baylor.edu/~donahoo/practical/CSockets/textcode.html TCP Echo Client */
 #include <stdio.h>      /* for printf() and fprintf() */
 #include <sys/socket.h> /* for socket(), connect(), send(), and recv() */
 #include <arpa/inet.h>  /* for sockaddr_in and inet_addr() */
@@ -5,72 +6,102 @@
 #include <string.h>     /* for memset() */
 #include <unistd.h>     /* for close() */
 
+#define PORT 13831      /* Port used */
 #define RCVBUFSIZE 32   /* Size of receive buffer */
+#define STACKSIZE 10    /* Size of stack used for calculations */
+int sock;               /* Socket descriptor */
+void DieWithError(char *errorMessage)
+{
+    perror(errorMessage);
+    exit(1);
+}
 
-void DieWithError(char *errorMessage);  /* Error handling function */
+void calculate(char *data)
+{
+    int value1, value2;             /* Values popped off stack */
+    char toSend[32];                /* String to be sent to server */
+    unsigned int toSendLen;         /* Length of string to be sent */
+    char buffer[RCVBUFSIZE];        /* Receiving buffer */
+    char *token;                    /* Token used for string tokenization */
+    
+    /* Stack Initialization */
+    int *stack;
+    int top;
+    stack = malloc(STACKSIZE*sizeof(int));
+    top = 0;
+
+    token = strtok(data, " ");
+    while(token != NULL)
+    {
+        if(strpbrk(token, "+-*/") != NULL) /* Operator. Pop off 2 values & get help from server */
+        {
+            if(top < 2)
+                DieWithError("Malformed RPN String");
+            value1 = stack[--top];
+            value2 = stack[--top];
+            sprintf(toSend, "%d %d %s", value1, value2, token);
+            toSendLen = strlen(toSend);
+
+            /* Send string to server */
+            if(send(sock, toSend, toSendLen, 0) != toSendLen)
+                DieWithError("send() sent a different number of bytes than expected!");
+            
+            /* Now get the answer from the server */
+            if(recv(sock, buffer, RCVBUFSIZE-1, 0) <= 0)
+                DieWithError("recv() failed or connection closed prematurely!");
+
+            /* Push value onto stack and continue */
+            stack[top++] = atoi(buffer);
+        }
+        else { /* Number. Push it onto the stack */
+            if(top == STACKSIZE)
+                DieWithError("Stack Overflow");
+            stack[top++] = atoi(token);
+        }
+        token = strtok(NULL, " ");
+    }
+    if(top > 1)
+        DieWithError("Malformed RPN String");
+    /* Send -1 to close connection on server */
+    if(send(sock, "-1", 2, 0) != 2)
+        DieWithError("send() sent a different number of bytes than expected!");
+    printf("%d\n", stack[0]);
+}
 
 int main(int argc, char *argv[])
 {
-    int sock;                        /* Socket descriptor */
-    struct sockaddr_in echoServAddr; /* Echo server address */
-    unsigned short echoServPort;     /* Echo server port */
-    char *servIP;                    /* Server IP address (dotted quad) */
-    char *echoString;                /* String to send to echo server */
-    char echoBuffer[RCVBUFSIZE];     /* Buffer for echo string */
-    unsigned int echoStringLen;      /* Length of string to echo */
-    int bytesRcvd, totalBytesRcvd;   /* Bytes read in single recv() 
-                                        and total bytes read */
+    struct sockaddr_in servAddr; /* Server address */
+    unsigned short servPort;     /* Server port */
+    char *servIP;                /* Server IP address (dotted quad) */
+    char *string;                /* String to send to server */
 
-    if ((argc < 3) || (argc > 4))    /* Test for correct number of arguments */
+
+    if (argc != 4)    /* Test for correct number of arguments */
     {
-       fprintf(stderr, "Usage: %s <Server IP> <Echo Word> [<Echo Port>]\n",
+       fprintf(stderr, "Usage: %s <Server IP> <Server Port> <RPN String>\n",
                argv[0]);
        exit(1);
     }
 
-    servIP = argv[1];             /* First arg: server IP address (dotted quad) */
-    echoString = argv[2];         /* Second arg: string to echo */
-
-    if (argc == 4)
-        echoServPort = atoi(argv[3]); /* Use given port, if any */
-    else
-        echoServPort = 7;  /* 7 is the well-known port for the echo service */
+    servIP = argv[1];          /* First arg: server IP address (dotted quad) */
+    servPort = atoi(argv[2]);  /* Second arg: server port */
+    string = argv[3];          /* Third arg: string to send to server */
 
     /* Create a reliable, stream socket using TCP */
     if ((sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
         DieWithError("socket() failed");
 
     /* Construct the server address structure */
-    memset(&echoServAddr, 0, sizeof(echoServAddr));     /* Zero out structure */
-    echoServAddr.sin_family      = AF_INET;             /* Internet address family */
-    echoServAddr.sin_addr.s_addr = inet_addr(servIP);   /* Server IP address */
-    echoServAddr.sin_port        = htons(echoServPort); /* Server port */
+    memset(&servAddr, 0, sizeof(servAddr));     /* Zero out structure */
+    servAddr.sin_family      = AF_INET;             /* Internet address family */
+    servAddr.sin_addr.s_addr = inet_addr(servIP);   /* Server IP address */
+    servAddr.sin_port        = htons(servPort); /* Server port */
 
     /* Establish the connection to the echo server */
-    if (connect(sock, (struct sockaddr *) &echoServAddr, sizeof(echoServAddr)) < 0)
+    if (connect(sock, (struct sockaddr *) &servAddr, sizeof(servAddr)) < 0)
         DieWithError("connect() failed");
-
-    echoStringLen = strlen(echoString);          /* Determine input length */
-
-    /* Send the string to the server */
-    if (send(sock, echoString, echoStringLen, 0) != echoStringLen)
-        DieWithError("send() sent a different number of bytes than expected");
-
-    /* Receive the same string back from the server */
-    totalBytesRcvd = 0;
-    printf("Received: ");                /* Setup to print the echoed string */
-    while (totalBytesRcvd < echoStringLen)
-    {
-        /* Receive up to the buffer size (minus 1 to leave space for
-           a null terminator) bytes from the sender */
-        if ((bytesRcvd = recv(sock, echoBuffer, RCVBUFSIZE - 1, 0)) <= 0)
-            DieWithError("recv() failed or connection closed prematurely");
-        totalBytesRcvd += bytesRcvd;   /* Keep tally of total bytes */
-        echoBuffer[bytesRcvd] = '\0';  /* Terminate the string! */
-        printf("%s", echoBuffer);      /* Print the echo buffer */
-    }
-
-    printf("\n");    /* Print a final linefeed */
+    
+    calculate(string);
 
     close(sock);
     exit(0);
--- Projects/one/RPNsrv-tcp.c
+++ Projects/one/RPNsrv-tcp.c
@@ -1,3 +1,4 @@
+/* Based on http://cs.baylor.edu/~donahoo/practical/CSockets/textcode.html TCP Echo Server */
 #include <stdio.h>      /* for printf() and fprintf() */
 #include <sys/socket.h> /* for socket(), bind(), and connect() */
 #include <arpa/inet.h>  /* for sockaddr_in and inet_ntoa() */
@@ -6,39 +7,99 @@
 #include <unistd.h>     /* for close() */
 
 #define MAXPENDING 5    /* Maximum outstanding connection requests */
+#define RCVBUFSIZE 32   /* Size of receive buffer */
 
-void DieWithError(char *errorMessage);  /* Error handling function */
-void HandleTCPClient(int clntSocket);   /* TCP client handling function */
+void DieWithError(char *errorMessage)
+{
+    perror(errorMessage);
+    exit(1);
+}
+
+void HandleTCPClient(int clntSocket)
+{
+    char buffer[RCVBUFSIZE];        /* Buffer for receving string */
+    int value1, value2, res;        /* Values used for calculations, and result */
+    char *operator;                 /* Operation to be done on value1 & 2 */
+    char *token;                    /* Used for string tokenization */
+    int toSendLen;                  /* Length of string to send */
+    char toSend[32];                /* Buffer for sending answer */
+    int bytesRcvd;                  /* Number of bytes received */
+    char *rcvd;                     /* String received */
+
+
+    /* Receive message from client */
+    if((bytesRcvd = recv(clntSocket, buffer, RCVBUFSIZE, 0)) < 0)
+        DieWithError("recv() failed");
+
+    rcvd = malloc(sizeof(char) * bytesRcvd);
+    strncpy(rcvd, buffer, bytesRcvd);
+    printf("Received: %s\n", rcvd);
+    while(strcmp(rcvd, "-1") != 0)
+    {
+        /* String will always be 3 tokens. Two integers and an operator */
+        token = strtok(rcvd, " ");
+        value1 = atoi(token); token = strtok(NULL, " ");
+        value2 = atoi(token); token = strtok(NULL, " ");
+        operator = token;
+        
+        /* Figure out what the operator is and perform calculation */
+        if(strcmp(token, "+") == 0)
+            res = value1 + value2;
+        else if(strcmp(token, "-") == 0)
+            res = value1 - value2;
+        else if(strcmp(token, "*") == 0)
+            res = value1 * value2;
+        else if(strcmp(token, "/") == 0)
+            res = value1 / value2;
+        
+        /* Send result */
+        printf("Sending: %d\n", res);
+        sprintf(buffer, "%d", res);
+        toSendLen = strlen(buffer);
+        if (send(clntSocket, buffer, toSendLen, 0) != strlen(buffer))
+           DieWithError("send() failed");
+
+         /* Receive message from client */
+        if((bytesRcvd = recv(clntSocket, buffer, RCVBUFSIZE, 0)) < 0)
+          DieWithError("recv() failed");
+
+        rcvd = malloc(sizeof(char) * bytesRcvd);
+        strncpy(rcvd, buffer, bytesRcvd);
+        printf("Received: %s\n", rcvd);
+    }
+    printf("Closing Connection.\n");
+    close(clntSocket);
+}
 
 int main(int argc, char *argv[])
 {
     int servSock;                    /* Socket descriptor for server */
     int clntSock;                    /* Socket descriptor for client */
-    struct sockaddr_in echoServAddr; /* Local address */
-    struct sockaddr_in echoClntAddr; /* Client address */
-    unsigned short echoServPort;     /* Server port */
+    struct sockaddr_in servAddr; /* Local address */
+    struct sockaddr_in clntAddr; /* Client address */
+    unsigned short servPort;     /* Server port */
     unsigned int clntLen;            /* Length of client address data structure */
-
+    
     if (argc != 2)     /* Test for correct number of arguments */
     {
         fprintf(stderr, "Usage:  %s <Server Port>\n", argv[0]);
         exit(1);
     }
 
-    echoServPort = atoi(argv[1]);  /* First arg:  local port */
+    servPort = atoi(argv[1]);  /* First arg:  local port */
 
     /* Create socket for incoming connections */
     if ((servSock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
         DieWithError("socket() failed");
       
     /* Construct local address structure */
-    memset(&echoServAddr, 0, sizeof(echoServAddr));   /* Zero out structure */
-    echoServAddr.sin_family = AF_INET;                /* Internet address family */
-    echoServAddr.sin_addr.s_addr = htonl(INADDR_ANY); /* Any incoming interface */
-    echoServAddr.sin_port = htons(echoServPort);      /* Local port */
+    memset(&servAddr, 0, sizeof(servAddr));   /* Zero out structure */
+    servAddr.sin_family = AF_INET;                /* Internet address family */
+    servAddr.sin_addr.s_addr = htonl(INADDR_ANY); /* Any incoming interface */
+    servAddr.sin_port = htons(servPort);      /* Local port */
 
     /* Bind to the local address */
-    if (bind(servSock, (struct sockaddr *) &echoServAddr, sizeof(echoServAddr)) < 0)
+    if (bind(servSock, (struct sockaddr *) &servAddr, sizeof(servAddr)) < 0)
         DieWithError("bind() failed");
 
     /* Mark the socket so it will listen for incoming connections */
@@ -48,16 +109,16 @@ int main(int argc, char *argv[])
     for (;;) /* Run forever */
     {
         /* Set the size of the in-out parameter */
-        clntLen = sizeof(echoClntAddr);
+        clntLen = sizeof(clntAddr);
 
         /* Wait for a client to connect */
-        if ((clntSock = accept(servSock, (struct sockaddr *) &echoClntAddr, 
+        if ((clntSock = accept(servSock, (struct sockaddr *) &clntAddr, 
                                &clntLen)) < 0)
             DieWithError("accept() failed");
 
         /* clntSock is connected to a client! */
 
-        printf("Handling client %s\n", inet_ntoa(echoClntAddr.sin_addr));
+        printf("Handling client %s\n", inet_ntoa(clntAddr.sin_addr));
 
         HandleTCPClient(clntSock);
     }
